4cf44548efeb6ff46180002cca45bea1bae54420
Flocke <FlareCO@users.noreply.github.com>
2019-02-07 22:09:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var request = require('request');
+
+exports.getRandomVid = function (key, keyname, callback){
+  var url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q='+keyname+'&key=' + key;
+  request(url, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+      var obj = JSON.parse(body);
+      obj = obj.items;
+
+      obj  = obj.filter(function (obj) {
+        return obj.id.kind == 'youtube#video';
+      });
+      var chosen = obj[Math.floor(Math.random() * obj.length)];
+      callback(null ,chosen);
+    }else  {
+      callback(body , null);
+    }
+  })
+}
